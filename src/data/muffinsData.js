98692de38e495d89869a54fc10187dbcfb777f45
sugandv21 <sdvsuganya@gmail.com>
2025-08-16 15:24:18 +0530

    Update muffinsData.js

--- src/data/muffinsData.js
+++ src/data/muffinsData.js
@@ -7,21 +7,51 @@ import muffin6 from "../assets/images/muffins6.jpg";
 
 export const muffins = [
   {
+    id: "64",
+    img: muffin4,
+    title: "Choco Indulgence Muffin",
+    price: 600,
+    description: "Decadent muffin made with rich cocoa and chocolate chunks.",
+    nutrition: "100g – 380 kcal, Fat 18g, Carbs 49g, Protein 6g",
+    ingredients: "Flour, Cocoa, Sugar, Eggs, Chocolate Chips",
+    allergies: "Contains Eggs, Milk, Gluten"
+  },
+  {
+    id: "66",
+    img: muffin6,
+    title: "Cranberry Burst Muffin",
+    price: 570,
+    description: "Moist muffin infused with tangy cranberries and citrus zest.",
+    nutrition: "100g – 345 kcal, Fat 15g, Carbs 47g, Protein 5g",
+    ingredients: "Flour, Sugar, Eggs, Cranberries, Orange Zest",
+    allergies: "Contains Eggs, Milk, Gluten"
+  },
+  {
     id: "61",
     img: muffin1,
-    title: "Blueberry Muffin",
+    title: "Fresh Blueberry Muffin",
     price: 550,
-    description: "Moist muffin loaded with juicy blueberries.",
+    description: "Soft and fluffy muffin packed with ripe blueberries.",
     nutrition: "100g – 340 kcal, Fat 14g, Carbs 48g, Protein 5g",
     ingredients: "Flour, Sugar, Eggs, Blueberries, Butter",
     allergies: "Contains Eggs, Milk, Gluten"
   },
   {
+    id: "65",
+    img: muffin5,
+    title: "Lemon Zest Muffin",
+    price: 580,
+    description: "Bright and citrusy muffin topped with poppy seeds.",
+    nutrition: "100g – 340 kcal, Fat 14g, Carbs 48g, Protein 5g",
+    ingredients: "Flour, Sugar, Eggs, Lemon, Poppy Seeds, Butter",
+    allergies: "Contains Eggs, Milk, Gluten"
+  },
+  {
     id: "62",
     img: muffin2,
-    title: "Chocolate Chip Muffin",
+    title: "Chocolate Delight Muffin",
     price: 570,
-    description: "Soft muffin studded with chocolate chips.",
+    description: "Light muffin baked with sweet chocolate chips.",
     nutrition: "100g – 360 kcal, Fat 15g, Carbs 50g, Protein 5g",
     ingredients: "Flour, Sugar, Eggs, Chocolate Chips, Butter",
     allergies: "Contains Eggs, Milk, Gluten"
@@ -29,41 +59,11 @@ export const muffins = [
   {
     id: "63",
     img: muffin3,
-    title: "Banana Walnut Muffin",
+    title: "Banana Nut Muffin",
     price: 590,
-    description: "Sweet banana muffin with crunchy walnuts.",
+    description: "Moist banana muffin with the crunch of walnuts.",
     nutrition: "100g – 350 kcal, Fat 16g, Carbs 47g, Protein 6g",
     ingredients: "Flour, Sugar, Bananas, Eggs, Walnuts, Butter",
     allergies: "Contains Eggs, Milk, Gluten, Nuts"
-  },
-  {
-    id: "64",
-    img: muffin4,
-    title: "Double Chocolate Muffin",
-    price: 600,
-    description: "Rich chocolate muffin with cocoa and chips.",
-    nutrition: "100g – 380 kcal, Fat 18g, Carbs 49g, Protein 6g",
-    ingredients: "Flour, Cocoa, Sugar, Eggs, Chocolate Chips",
-    allergies: "Contains Eggs, Milk, Gluten"
-  },
-  {
-    id: "65",
-    img: muffin5,
-    title: "Lemon Poppyseed Muffin",
-    price: 580,
-    description: "Zesty lemon muffin with crunchy poppy seeds.",
-    nutrition: "100g – 340 kcal, Fat 14g, Carbs 48g, Protein 5g",
-    ingredients: "Flour, Sugar, Eggs, Lemon, Poppy Seeds, Butter",
-    allergies: "Contains Eggs, Milk, Gluten"
-  },
-  {
-    id: "66",
-    img: muffin6,
-    title: "Cranberry Orange Muffin",
-    price: 570,
-    description: "Tangy cranberry and orange-flavored muffin.",
-    nutrition: "100g – 345 kcal, Fat 15g, Carbs 47g, Protein 5g",
-    ingredients: "Flour, Sugar, Eggs, Cranberries, Orange Zest",
-    allergies: "Contains Eggs, Milk, Gluten"
   }
 ];
