e958868dfbbdd16cbe96db6c8008576bd10d2be8
sugandv21 <sdvsuganya@gmail.com>
2025-08-16 08:27:34 +0530

    Update AuthPage.jsx

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -1,15 +1,22 @@
-import React, { useState } from "react";
+import React, { useEffect , useState } from "react";
 import AuthTabs from "../components/AuthTabs";
 import Login from "../components/Login";
 import Register from "../components/Register";
 
 export default function AuthPage() {
   const [activeTab, setActiveTab] = useState("login");
+    useEffect(() => {
+      document.title = "CHERRI | lOGIN | REGISTER";  
+    }, []);
 
   return (
-    <div className="mt-40 max-w-5xl mx-auto mt-10 bg-white rounded-lg overflow-hidden">
+    <div className="mt-24 md:mt-56 lg:mt-40 max-w-5xl mx-auto rounded-lg overflow-hidden bg-[#FFF8F0]">
       <AuthTabs activeTab={activeTab} setActiveTab={setActiveTab} />
-      {activeTab === "login" ? <Login /> : <Register />}
+      {activeTab === "login" ? (
+        <Login setActiveTab={setActiveTab} />
+      ) : (
+        <Register setActiveTab={setActiveTab} />
+      )}
     </div>
   );
 }
