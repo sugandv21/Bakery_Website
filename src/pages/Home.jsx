417847b3b379142d88dc119299ddc31df1b96243
sugandv21 <sdvsuganya@gmail.com>
2025-08-16 08:33:57 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import BannerSlider from "../components/BannerSlider";
 import WhyChooseUs from "../components/WhyChooseUs";
 import CommonQuestions from "../components/CommonQuestions";
@@ -6,8 +6,11 @@ import bannerImage from "../assets/images/Frame 6.png";
 import CustomerTestimonials from "../components/CustomerTestimonials";
 
 function Home() {
+   useEffect(() => {
+      document.title = "CHERRI | HOME";  
+    }, []);
   return (
-    <div className=" mt-36">
+    <div className="mt-24 md:mt-52 lg:mt-28 bg-[#FFF8F0] ">
       <BannerSlider />
       <WhyChooseUs />
       <h2 className="text-center text-2xl sm:text-3xl font-bold text-[#e57f35] mb-8">
